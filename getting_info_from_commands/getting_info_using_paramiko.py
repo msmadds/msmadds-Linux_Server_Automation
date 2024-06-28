c043fa0c82d249074bdd26a8b6f552df9adfcc11
msmadds <103424445+msmadds@users.noreply.github.com>
2024-06-28 13:04:36 +0300

    Update getting_info_linux_using_paramiko.py
    Update getting_info_linux_using_paramiko.py

--- getting_info_from_commands/getting_info_using_paramiko.py
+++ getting_info_from_commands/getting_info_using_paramiko.py
@@ -2,6 +2,7 @@ import paramiko
 from paramiko import ssh_exception
 import os
 import  getpass
+import time
 
 #give the script a full vlan range, and the script test to see if an IP is availabe by ping, in that range
 #and for the IP that is available  by ping to ssh into the Linux server and run the linux command to get the info on that machine 
@@ -24,33 +25,49 @@ def get_info(hostname, password, username, port, commands):
                 for command in commands:
                     stdin,stdout,stderr = ssh.exec_command(command)
                     lines = stdout.readlines()
-                    with open(host+'.txt', 'a') as f: #open the file to store the output, naming the  file  by using the hostname
-                        f.write('*'*30 + f"{command}" + "*"*30)
-                        f.write('\n')
-                        for line in lines:
-                            f.write(line)
+                    try:
+                        os.makedirs(hostname)
+                        file_ = hostname + '/'+host+'.txt'
+                        with open(file_, 'a') as f: #open the file to store the output, naming the  file  by using the hostname
+                            f.write('*'*30 + f"{command}" + "*"*30)
+                            f.write('\n')
+                            for line in lines:
+                                f.write(line)
+                    except FileExistsError:
+                        file_ = hostname + '/'+host+'.txt'
+                        with open(file_, 'a') as f: #open the file to store the output, naming the  file  by using the hostname
+                            f.write('*'*30 + f"{command}" + "*"*30)
+                            f.write('\n')
+                            for line in lines:
+                                f.write(line)
+                        
+                        
                             
             except ssh_exception.AuthenticationException: 
-                #capture the paramiko authentication exception
-                print("Authentication failed, check credentials")
+                #capture the paramiko authenti cation exception
+                print(f"Authentication failed for {host}, check credentials")
                 
 def  check_ip_availability(hostname):
         
     #check if the the IP  is available by ping
-    response = os.system("ping -c 1 " + hostname + "> /Users/User/Documents/AUTOMATION/output.txt" ) 
-     #if the IP is available the response of the ping will be 0, so the below block will be executed
+    response = os.system("ping -c 1 " + hostname + "> /Users/madelinehaule/Documents/AUTOMATION/SERVERS_AUTO/output.txt" ) 
+     #if the IP is available the response of the ping will be 0
     return response
     
 
 def main():
     #main function
     port = 22
-    username = input("Input the username")
+    username = input("Input the username: ")
     password = getpass.getpass()
     commands =['cat /etc/debian_version', 'cat /etc/passwd', 'ls']
     hostname = "xx.xx.xx." #input the three octacte of your IP 
+    start = time.time()
     get_info(hostname, password, username, port, commands)
-    
-    
+    end= time.time()
+    total_time = end - start
+    print(str(total_time))
+
+
 if __name__ == "__main__":
     main()
